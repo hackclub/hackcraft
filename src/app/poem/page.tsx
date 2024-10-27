7f63961ab5205f2307aebde5967011123a74b794
Marios <xrteach@gmail.com>
2024-10-27 11:20:28 +0200

    feat: poem

--- src/app/poem/page.tsx
+++ src/app/poem/page.tsx
@@ -2,31 +2,75 @@
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+import "./page.css";
+
 export default function Page() {
-    const searchParams = useSearchParams();
-    const userId = searchParams.get("id");
-    const [username, setUsername] = useState("Steve");
+  const searchParams = useSearchParams();
+  const userId = searchParams.get("id");
+  const [username, setUsername] = useState("Steve");
 
-    useEffect(() => {
-        if (userId) {
-            const url = new URL('/api/getUser', window.location.origin);
-            url.searchParams.append("id", userId);
+  useEffect(() => {
+    if (userId) {
+      const url = new URL('/api/getUser', window.location.origin);
+      url.searchParams.append("id", userId);
 
-            fetch(url.toString(), {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-            }).then((res) => res.json()).then((data) => {
-                setUsername(data.username);
-            });
-        }
-    }, [userId]);
+      fetch(url.toString(), {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+      }).then((res) => res.json()).then((data) => {
+        setUsername(data.username);
+      });
+    }
+  }, [userId]);
 
-    return (
-        <div>
-            <h1>Page</h1>
-            <p>Welcome, {username}!</p>
-        </div>
-    );
+  gsap.registerPlugin(ScrollTrigger, useGSAP);
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    gsap.utils.toArray(".poem-text").forEach(e => tl.to(e, {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: e,
+        markers: true,
+        start: "top center",
+        end: "+=24 center",
+        scrub: 1,
+        toggleActions: "play reverse play reset"
+      }
+    }));
+
+    tl.to(".mc-ysws-logo", {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: "img",
+        markers: true,
+        start: "top center
+        end: "top top",
+        scrub: 1,
+        toggleActions: "play pause none none"
+      }
+    });
+
+    setInterval(() => window.scrollBy(0, 1.5), 50)
+  })
+
+  return (
+    <div id="poem">
+      <div className="poem">
+        <p className="poem-text player1">I see the game you mean.</p>
+        <p className="poem-text player2">Minecraft?</p>
+        <p className="poem-text player1">Yes. Take care. It has reached a higher level now. It can read our desires.</p>
+        <p className="poem-text player2">You can do whatever you want. There's unlimited freedom.</p>
+        <p className="poem-text player1">It reads our thoughts like they were code on a screen.</p>
+        <p className="poem-text player2">Go out and make something cool</p>
+        <img src="images/logo.png" className="mc-ysws-logo" />
+      </div>
+    </div>
+  );
 }
